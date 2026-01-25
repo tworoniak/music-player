52a533b00b15801b7b11a6f00d7147f4a7ad1d2d
Thomas Woroniak <thomasworoniak@gmail.com>
2026-01-25 17:16:17 -0600

    add content

--- src/types/tracks.ts
+++ src/types/tracks.ts
@@ -27,6 +27,24 @@ const tracksData: Track[] = [
     coverUrl: "/images/Whitechapel-Mark_of_the_Blade.jpg",
     audioUrl: "/music/02_Mark_of_the_Blade.mp3",
   },
+  {
+    id: 3,
+    title: "Tyhjyys",
+    artist: "Wolfheart",
+    album: "Tyhjyys",
+    duration: 240,
+    coverUrl: "/images/Wolfheart-Tyhjyys.jpg",
+    audioUrl: "/music/08_Tyhjyys.mp3",
+  },
+  {
+    id: 4,
+    title: "Sorceress",
+    artist: "Opeth",
+    album: "Sorceress",
+    duration: 300,
+    coverUrl: "/images/Opeth_Sorceress_PromoCover_revised.jpg",
+    audioUrl: "/music/02_Sorceress.mp3",
+  },
 ];
 
 export default tracksData;
